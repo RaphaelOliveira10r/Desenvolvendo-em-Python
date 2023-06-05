f62f71804912243666635ca86bfd492e94cc64fd
RaphaelOliveira10r <129516305+RaphaelOliveira10r@users.noreply.github.com>
2023-06-05 08:40:32 -0300

    Praticando
    funções decoradoras - saudação

---
+++ Exercícios/patricando2.py
@@ -0,0 +1,13 @@
+# Funções decoradoras
+
+
+def equacao_1_grau(x,y,z):
+    return x + y * z ** 4
+
+def decorador(funcao):
+    return funcao
+
+
+
+equacao = decorador(equacao_1_grau(2,4,6))
+print(equacao)
---
+++ Exercícios/praticando1.py
@@ -0,0 +1,15 @@
+# Funções decoradoras 
+
+
+def saudacao(saudar):
+    return saudar
+
+
+
+def func(funcao):
+    return funcao
+
+
+print(func(saudacao('Bom dia')))
+print(func(saudacao('Boa Tarde')))
+print(func(saudacao('Boa Noite')))
