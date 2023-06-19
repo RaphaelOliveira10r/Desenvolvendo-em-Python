b45dd382d11f708fb28723fe098d37434ad4a006
RaphaelOliveira10r <129516305+RaphaelOliveira10r@users.noreply.github.com>
2023-06-19 00:50:41 -0300

    Update praticando13.py

--- Exercícios/praticando13.py
+++ Exercícios/praticando13.py
@@ -1,3 +1,18 @@
 # mapeando lista com dic
 
-exemplo = [{'produto':'','preço':1},]
+exemplo = [
+    {'produto':'Geladeira','preço':1500},
+    {'produto':'Fogão','preço':899},
+    {'produto':'Microondas','preço':450},
+    {'produto':'Televisão','preço':1200},
+    {'produto':'Notebook','preço':2500},
+    {'produto':'Cama','preço':1199},
+    {'produto':'Mesa','preço':250},
+    {'produto':'Ferro de passar','preço':80},
+    {'produto':'Liquidificador','preço':120},
+    {'produto':'Forno Elétrico','preço':349},
+    ]
+
+
+mapeando_precos = map (lambda x:x['preço'],exemplo)
+print(list(mapeando_precos))
