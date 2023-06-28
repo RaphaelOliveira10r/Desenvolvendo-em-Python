37aa2cc280aed2cc4a0d3af7f698bbf09b955cf1
RaphaelOliveira10r <129516305+RaphaelOliveira10r@users.noreply.github.com>
2023-06-28 00:48:05 -0300

    Update praticando9.py

--- Exercícios/praticando9.py
+++ Exercícios/praticando9.py
@@ -2,6 +2,12 @@
 # construa um algoritmo que calcule seu peso ideal,
 # usando a seguinte fórmula: (72.7*altura) - 58
 
-altura = float(input("Digite a sua altura em metros: "))
-peso = (72.7 * altura) - 58
-print(f"Seu peso ideal é: {peso}")
+# altura = float(input("Digite a sua altura em metros: "))
+# peso = (72.7 * altura) - 58
+# print(f"Seu peso ideal é: {peso}")
+
+def peso_ideal(altura=float):
+    peso = (72.7 * altura) - 58
+    return f"Seu peso ideal é: {peso}"
+
+print(peso_ideal(altura=1.85))
