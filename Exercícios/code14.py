5066dc578ece5a59c73d6c011e8618a375469c6d
RaphaelOliveira10r <129516305+RaphaelOliveira10r@users.noreply.github.com>
2023-07-03 12:51:08 -0300

    commit

---
+++ Exercícios/code14.py
@@ -0,0 +1,11 @@
+# exercício inverter listas
+
+
+x = [1,2,3,4,5,6,7,8,9,10]
+
+def inverter_listas(lista):
+    return lista[::-1]
+
+
+
+print(x)
--- Exercícios/praticando34.py
+++ Exercícios/praticando34.py
@@ -1,3 +1,4 @@
+# Praticando classe com polimofirmo na identificação
 class Pessoa:
     def __init__(self,nome,sobrenome,cpf):
         self.__nome = nome
@@ -7,23 +8,36 @@ class Pessoa:
     def nome_completo(self):
         return f'{self.__nome} {self.__sobrenome}'
     
-    def get_identificacao(self):
-        return f'{self.__cpf}'
+    def identificacao(self):
+        return self.__cpf
     
+
 
 class Cliente(Pessoa):
     def __init__(self, nome, sobrenome, cpf,codigo):
-        super().__init__(nome, sobrenome, cpf,codigo)
+        super().__init__(nome, sobrenome, cpf)
         self.__codigo = codigo
 
-    def get_identificacao(self):
-        return f'{self.__codigo}'
+    def identificacao(self):
+        return self.__codigo
     
+
 
 class Funcionario(Pessoa):
     def __init__(self, nome, sobrenome, cpf,matricula):
-        super().__init__(nome, sobrenome, cpf,matricula)
+        super().__init__(nome, sobrenome, cpf)
         self.__matricula = matricula
     
-    def get_identificacao(self):
-        return f'{self.__matricula}'
+    def identificacao(self):
+        return self.__matricula
+    
+
+c1 = Cliente("Maria","Silva","451236789010","1234-5")
+print(c1.nome_completo())
+print(c1.identificacao())
+
+print()
+f1 = Funcionario("José","Lopes","069873215412","5462-9")
+
+print(f1.nome_completo())
+print(f1.identificacao())
