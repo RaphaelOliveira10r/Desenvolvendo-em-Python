150a130b60d319901fcb1f1aeed4549594a0d446
RaphaelOliveira10r <129516305+RaphaelOliveira10r@users.noreply.github.com>
2023-07-11 16:33:29 -0300

    Commit
    Exercitando funções

---
+++ Exercícios/code21.py
@@ -0,0 +1,8 @@
+
+
+def soma():
+    soma = 2+4
+    print(soma)
+
+
+soma()
---
+++ Exercícios/code22.py
@@ -0,0 +1,11 @@
+def soma(x,y):
+    return x+y
+
+
+
+print(soma(y=89,x=13))
+
+print(soma(198,y=98))
+
+
+print(soma(x=10,y=1))
