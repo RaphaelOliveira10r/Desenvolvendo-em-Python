bbb336ce02c30c3fedc07e706902eb36b6509cb4
RaphaelOliveira10r <129516305+RaphaelOliveira10r@users.noreply.github.com>
2023-07-13 15:02:28 -0300

    Update praticando38.py

--- Exercícios/praticando38.py
+++ Exercícios/praticando38.py
@@ -1,9 +1,10 @@
 
 class Exercito:
-    def __init__(self,pais,ranking,quantidade):
+    def __init__(self,pais,ranking,quantidade,localizacao):
         self._pais = pais
         self._ranking = ranking
         self._quantidade = quantidade
+        self._localizacao = localizacao
 
     def get_pais(self):
         return f'País: {self._pais}'
@@ -15,7 +16,27 @@ class Exercito:
         return f'Contingente: {self._quantidade} mil soldados ativos!'
     
 
-brasil = Exercito("Brasil",10,200)
-print(brasil.get_pais())
-print(brasil.get_ranking())
-print(brasil.get_quantidade())
+class Marinha(Exercito):
+    def __init__(self, pais, ranking, quantidade, localizacao,navios):
+        super().__init__(pais, ranking, quantidade, localizacao)
+        self._navios = navios
+    
+    def get_navios(self):
+        return f'Quantidade de Navios:{self._navios}'
+
+
+class Aeronautica(Exercito):
+    def __init__(self, pais, ranking, quantidade, localizacao,avioes):
+        super().__init__(pais, ranking, quantidade, localizacao)
+        self._avioes = avioes
+
+    def get_avioes(self):
+        return f'Quantidade de Aviões:{self._avioes}'
+    
+
+
+ebr = Marinha('Brasil',10,200,'America do Sul',134)
+print(ebr.get_pais())
+print(ebr.get_quantidade())
+print(ebr.get_navios())
+print(ebr.get_ranking())
